ff609d0bc526a6ed58a8ec38b8648ac3add07641
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2024-11-13 09:42:01 +0100

    added board model

---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+from puzzle import Action, Board
+import numpy as np
+
+SIZE = 3
+
+
+def main():
+    solution = np.arange(SIZE**2)
+    solution = np.roll(solution, -1).reshape(SIZE, SIZE)
+
+    p = Board(SIZE, 0)  # get a radom board
+    print(p)
+    while True:
+        action = Action(np.random.choice(np.nonzero(p.valid_actions)[0]))
+        print()
+        print(action)
+        p = p.act(action)
+        print(p)
+
+
+if __name__ == "__main__":
+    main()
---
+++ puzzle.py
@@ -0,0 +1,84 @@
+from re import A
+from typing import Self
+import numpy as np
+from enum import Enum
+from icecream import ic
+
+
+class Action(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+
+DIRECTIONS = {
+    Action.UP: (-1, 0),
+    Action.DOWN: (1, 0),
+    Action.LEFT: (0, -1),
+    Action.RIGHT: (0, 1),
+}
+
+
+class InvalidMoveError(Exception):
+    pass
+
+
+class Board:
+    def __init__(self, board: np.ndarray | list[list[int]] | int, seed=0):
+        rng = np.random.Generator(np.random.PCG64(seed))
+        if isinstance(board, int):
+            if board < 2:
+                raise ValueError("Board size must be at least 2x2")
+            board = np.arange(board**2).reshape(board, board)
+            board = rng.permutation(board)
+
+        if isinstance(board, list):
+            board = np.array(board)
+
+        self.m = board
+        self.size = board.shape[0]
+        self.position = tuple(x[0] for x in np.nonzero(board == 0))
+        self.valid_actions = self._get_valid_actions()
+
+    def _get_valid_actions(self) -> list[Action]:
+        actions = np.zeros((4,), dtype=bool)
+
+        if self.position[0] != 0:
+            actions[Action.UP.value] = True
+
+        if self.position[1] != 0:
+            actions[Action.LEFT.value] = True
+
+        if self.position[0] != self.size - 1:
+            actions[Action.DOWN.value] = True
+
+        if self.position[1] != self.size - 1:
+            actions[Action.RIGHT.value] = True
+
+        return actions
+
+    def act(self, action: Action) -> Self:
+        if not self.valid_actions[action.value]:
+            raise InvalidMoveError
+
+        new_pos = (
+            self.position[0] + DIRECTIONS[action][0],
+            self.position[1] + DIRECTIONS[action][1],
+        )
+
+        new_board = self.m.copy()
+        new_board[self.position], new_board[new_pos] = (
+            new_board[new_pos],
+            new_board[self.position],
+        )
+        return Board(new_board)
+
+    def __hash__(self):
+        return hash(self.m.tobytes())
+
+    def __str__(self):
+        return str(self.m)
+
+    def __repr__(self):
+        return str(self)

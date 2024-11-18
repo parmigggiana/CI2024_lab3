maybe fixed linear conflict calculation

custom_heuristics.py
@@ -3,6 +3,10 @@ import numpy as np
 from puzzle import Board
 
 
+#   0 1 2
+# 0 1 2 3
+# 1 4 5 6
+# 2 7 8 9
 def improved_manhattan(board_size, weights=[1, 1, 1]):
     solution = np.arange(board_size**2)
     solution = np.roll(solution, -1).reshape(board_size, board_size)
@@ -11,17 +15,31 @@ def improved_manhattan(board_size, weights=[1, 1, 1]):
     def manhattan(board: Board):
         return board.manhattan_distance(solution)
 
-    def conflicts(board: Board):
+    def linear_conflicts(board: Board):
         conflicts = 0
-        for i in range(board.size):
-            for j in range(board.size):
-                if board[i][j] == 0:
+        for row in range(board.size):
+            for column in range(board.size):
+                if board[row][column] == 0:
                     continue
-                for k in range(i, board.size):
-                    for l in range(j, board.size):
-                        if board[k][l] == 0:
+                # check if board[i][j] is in the correct column
+                if board[row][column] % board.size == column - 1:
+                    for i in range(row, board.size):
+                        if board[i][column] == 0:
                             continue
-                        if board[i][j] > board[k][l] and (i == k or j == l):
+                        if (
+                            board[row][column] % board.size != column - 1
+                            and board[row][column] > board[i][column]
+                        ):
+                            conflicts += 1
+
+                # check if board[i][j] is in the correct row
+                if (board[row][column] - 1) // board.size == row:
+                    for i in range(column, board.size):
+                        if board[row][i] == 0:
+                            continue
+                        if (board[row][column] - 1) // board.size != row and board[row][
+                            column
+                        ] > board[row][i]:
                             conflicts += 1
         return conflicts
 
@@ -39,6 +57,6 @@ def improved_manhattan(board_size, weights=[1, 1, 1]):
 
     return (
         lambda board: manhattan(board) * weights[0]
-        + conflicts(board) * weights[1]
+        + linear_conflicts(board) * weights[1]
         + inversions(board) * weights[2]
     )
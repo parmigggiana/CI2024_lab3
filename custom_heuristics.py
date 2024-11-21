1e6390489224f939975f7a64a2cac57c44848b70
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2024-11-21 09:52:10 +0100

    simplified code, added weights for 3x3 boards

--- custom_heuristics.py
+++ custom_heuristics.py
@@ -3,10 +3,51 @@ import numpy as np
 from puzzle import Board
 
 
-#   0 1 2
-# 0 1 2 3
-# 1 4 5 6
-# 2 7 8 9
+def linear_conflicts(board: Board):
+    size = board.size
+    conflicts = 0
+
+    # Check for row conflicts
+    for row in range(size):
+        for col in range(size):
+            tile = board[row][col]
+            if tile == 0:
+                continue
+
+            # Check if the tile is in the correct row
+            if (tile - 1) // size == row:
+                for i in range(col + 1, size):
+                    other_tile = board[row][i]
+                    if other_tile == 0:
+                        continue
+                    if (other_tile - 1) // size == row and tile > other_tile:
+                        conflicts += 1
+
+            # Check if the tile is in the correct column
+            if tile % size == col - 1:
+                for i in range(row + 1, size):
+                    other_tile = board[i][col]
+                    if other_tile == 0:
+                        continue
+                    if other_tile % size == col and tile > other_tile:
+                        conflicts += 1
+
+    return conflicts
+
+
+def inversions(board: Board):
+    inversions = 0
+    for i in range(board.size):
+        for j in range(board.size):
+            if board[i][j] == 0:
+                continue
+            for k in range(i, board.size):
+                for l in range(j, board.size):
+                    if board[k][l] != 0 and board[i][j] > board[k][l]:
+                        inversions += 1
+    return inversions
+
+
 def improved_manhattan(board_size, weights=None):
     solution = np.arange(board_size**2)
     solution = np.roll(solution, -1).reshape(board_size, board_size)
@@ -15,60 +56,18 @@ def improved_manhattan(board_size, weights=None):
     if weights is None:
         match board_size:
             case 3:
-                weights = [1, 0, 2]
+                weights = (0.21, 0.67, 0.12)
             case 4:
-                weights = [0.9, 0.25, 1.5]
+                raise NotImplementedError(
+                    "Default weights for 4x4 boards are not implemented"
+                )
             case 5:
-                weights = [1, 1, 1]
-
-    def manhattan(board: Board):
-        return board.manhattan_distance(solution)
-
-    def linear_conflicts(board: Board):
-        size = board.size
-        conflicts = 0
-
-        # Check for row conflicts
-        for row in range(size):
-            for col in range(size):
-                tile = board[row][col]
-                if tile == 0:
-                    continue
-
-                # Check if the tile is in the correct row
-                if (tile - 1) // size == row:
-                    for i in range(col + 1, size):
-                        other_tile = board[row][i]
-                        if other_tile == 0:
-                            continue
-                        if (other_tile - 1) // size == row and tile > other_tile:
-                            conflicts += 1
-
-                # Check if the tile is in the correct column
-                if tile % size == col - 1:
-                    for i in range(row + 1, size):
-                        other_tile = board[i][col]
-                        if other_tile == 0:
-                            continue
-                        if other_tile % size == col and tile > other_tile:
-                            conflicts += 1
-
-        return conflicts
-
-    def inversions(board: Board):
-        inversions = 0
-        for i in range(board.size):
-            for j in range(board.size):
-                if board[i][j] == 0:
-                    continue
-                for k in range(i, board.size):
-                    for l in range(j, board.size):
-                        if board[k][l] != 0 and board[i][j] > board[k][l]:
-                            inversions += 1
-        return inversions
+                raise NotImplementedError(
+                    "Default weights for 5x5 boards are not implemented"
+                )
 
     return (
-        lambda board: manhattan(board) * weights[0]
+        lambda board: board.manhattan_distance(solution) * weights[0]
         + linear_conflicts(board) * weights[1]
         + inversions(board) * weights[2]
     )
